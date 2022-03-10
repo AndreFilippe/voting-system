c9a336fe48afcc2f23f4b63bc2ff823613036428
André Filippe dos Santos <fellipe.mrt@gmail.com>
2022-03-10 16:57:06 -0300

    test: created test case to projetec

---
+++ tests/TestCase.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Test;
+
+use Carbon\Carbon;
+use PHPUnit\Framework\TestCase as FrameworkTestCase;
+
+abstract class TestCase extends FrameworkTestCase
+{
+    public function __construct()
+    {
+        parent::__construct();
+        require __DIR__ . '/../bootstrap/app.php';
+    }
+
+    protected function getProperty(&$object, string $property): mixed
+    {
+        $reflection = new \ReflectionClass(get_class($object));
+        $test = $reflection->getProperty($property);
+        $test->setAccessible(true);
+        return $test->getValue($object);
+    }
+
+    protected function setDateTime(string $dateTime): void
+    {
+        Carbon::setTestNow($dateTime);
+    }
+}
